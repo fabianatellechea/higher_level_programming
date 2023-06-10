b1a536ca4aef953520097b5a18dd5cb981564a69
fabianatellechea <fabianatellechea@gmail.com>
2023-06-10 10:28:13 -0700

    rm return

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     list = []
     for i in my_list:
         if i % 2 == 0:
-            return list.append(True)
+            list.append(True)
         else:
-            return list.append(False)
+            list.append(False)
     return list
