9e1e1df7f19c9106d054859ea1aff6e11aad5f91
fabianatellechea <fabianatellechea@gmail.com>
2023-06-10 10:41:03 -0700

    if added

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
-    for i in my_list:
-        if i > max:
-            max = i
-    return max
-
-    if len(my_list) == 0:
-        return None
+    if my_list:
+        max = my_list[0]
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
+        if len(my_list) == 0:
+            return None
