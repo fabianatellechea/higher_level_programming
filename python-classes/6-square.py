9e3f22c106a874fdf950e1135713d055b279b728
fabianatellechea <fabianatellechea@gmail.com>
2023-06-13 09:14:06 -0700

    border cases

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -30,7 +30,7 @@ class Square:
     def position(self, value):
         self.__position = value
 
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or value[0] < 0 or value[1] < 0 or value[0] is not int or value[1] is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
