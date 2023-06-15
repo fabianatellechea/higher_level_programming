f49aafc22b0a029735bdae434a710f2a77f7b3d2
fabianatellechea <fabianatellechea@gmail.com>
2023-06-15 12:08:48 -0700

    init errors

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,6 +10,15 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+        if type(self.__width) is not int:
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+        if type(self.__height) is not int:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+
     @property
     def width(self):
         return self.__width
