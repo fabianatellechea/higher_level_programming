0b7c0dbca7b2b3e9b71ee99a518b41888677cd45
fabianatellechea <fabianatellechea@gmail.com>
2023-06-15 13:03:22 -0700

    rstrip()

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,4 +56,5 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ("")
-        return ("#" * self.__width + "\n") * self.__height
+
+        return (("#" * self.__width + "\n") * self.__height).rstrip()
