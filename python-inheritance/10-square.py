f760b814d9f2384610f2e13dd277af6bde34cdd9
fabianatellechea <fabianatellechea@gmail.com>
2023-06-22 10:42:04 -0700

    order?

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,9 +7,9 @@ class Square(Rectangle):
     """class """
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
